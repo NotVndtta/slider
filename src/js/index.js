0ddf11daa059ccc091556a4df6d24d1700075abf
NotVndtta <thenotalexey192@gmail.com>
2023-09-13 00:05:24 +0300

    Swiper and first slide

--- src/js/index.js
+++ src/js/index.js
@@ -1,2 +1,36 @@
-import mobileNav from './modules/mobile-nav.js';
-mobileNav();
+// import mobileNav from './modules/mobile-nav.js';
+// mobileNav();
+
+// import Swiper bundle with all modules installed
+import Swiper from 'swiper/bundle';
+
+// import styles bundle
+import 'swiper/css/bundle';
+
+// init Swiper:
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    // direction: 'vertical',
+    loop: false,
+    parallax: true,
+    speed: 1000,
+
+    keyboard: {
+        enabled: true,
+    },
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  
+    // And if we need scrollbar
+    scrollbar: {
+      el: '.swiper-scrollbar',
+    },
+  });
